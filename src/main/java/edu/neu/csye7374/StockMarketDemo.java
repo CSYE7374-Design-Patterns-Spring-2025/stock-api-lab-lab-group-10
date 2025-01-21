3fdeb8099df99cd9a771ed1a8f25859cc70a7c7e
TirthNaik-99 <naik.ti@northeastern.edu>
2025-01-21 15:39:30 -0500

    Updated StockMarketDemo

--- src/main/java/edu/neu/csye7374/StockMarketDemo.java
+++ src/main/java/edu/neu/csye7374/StockMarketDemo.java
@@ -4,18 +4,18 @@ public class StockMarketDemo {
     public static void demo() {
         StockMarket stockMarket = StockMarket.getInstance();
         AmazonStock amazonStock = new AmazonStock("Amazon", 150.51, "Amazon Inc.");
-        ChewyStock netflixStock = new ChewyStock("Netflix", 200.23, "Netflix Inc.");
+        ChewyStock chewyStock = new ChewyStock("Chewy", 200.23, "Chewy Inc.");
         AmexStock amexStock = new AmexStock("AMX", 100.0, "American Express", 0.10);
         AppleStock appleStock = new AppleStock("AAPL", 200.0, "Nvidia Corporation", 0.25);
         stockMarket.addStock(amazonStock);
-        stockMarket.addStock(netflixStock);
+        stockMarket.addStock(chewyStock);
         stockMarket.addStock(amexStock);
         stockMarket.addStock(appleStock);
         System.out.println("Current stocks added to the stock market: ");
         stockMarket.showAllStocks();
         System.out.println();
         double[] amazonBids = new double[]{155.0, 140.0, 138.0, 145.0, 150.0, 155.0};
-        double[] netflixBids = new double[]{205.0, 190.0, 195.0, 210.0, 215.0, 200.0};
+        double[] chewyBids = new double[]{205.0, 190.0, 195.0, 210.0, 215.0, 200.0};
         double[] amexBids = new double[]{105.0, 102.0, 108.0, 106.0, 110.0, 107.0};
         double[] appleBids = new double[]{205.0, 202.0, 208.0, 206.0, 210.0, 207.0};
         System.out.println("Starting trading Amazon Stock");
@@ -23,24 +23,24 @@ public class StockMarketDemo {
         int var7 = amazonBids.length;
 
         int var8;
-        double netflixBid;
+        double chewyBid;
         for(var8 = 0; var8 < var7; ++var8) {
-            netflixBid = var6[var8];
-            stockMarket.tradeStock(amazonStock, netflixBid);
+            chewyBid = var6[var8];
+            stockMarket.tradeStock(amazonStock, chewyBid);
             System.out.println(amazonStock);
             System.out.println("Stock metric after bid: " + amazonStock.getMetric());
         }
 
         System.out.println();
-        System.out.println("Starting trading Netflix Stock");
-        var6 = netflixBids;
-        var7 = netflixBids.length;
+        System.out.println("Starting trading Chewy Stock");
+        var6 = chewyBids;
+        var7 = chewyBids.length;
 
         for(var8 = 0; var8 < var7; ++var8) {
-            netflixBid = var6[var8];
-            stockMarket.tradeStock(netflixStock, netflixBid);
-            System.out.println(netflixStock);
-            System.out.println("Stock metric after bid: " + netflixStock.getMetric());
+            chewyBid = var6[var8];
+            stockMarket.tradeStock(chewyStock, chewyBid);
+            System.out.println(chewyStock);
+            System.out.println("Stock metric after bid: " + chewyStock.getMetric());
         }
 
         System.out.println();
@@ -65,7 +65,7 @@ public class StockMarketDemo {
         System.out.println();
         System.out.println("All stocks in the market with current price after bids: ");
         stockMarket.showAllStocks();
-        stockMarket.removeStock(netflixStock);
+        stockMarket.removeStock(chewyStock);
         System.out.println();
         System.out.println("Current stocks in stock market after removing a stock: ");
         stockMarket.showAllStocks();
